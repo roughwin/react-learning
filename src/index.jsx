de41b1c045d44ef54c43317fc15dbcaad0daf7d2
roughwin <roughwin@qq.com>
2017-05-15 09:46:07 +0800

    init

---
+++ src/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { render } from 'react-dom';
+import moment from 'moment';
+import { Alert } from 'antd';
+import 'moment/locale/zh-cn';
+
+const rootEl = document.getElementById('root');
+
+let el = (<div>hello</div>);
+
+/*render((<div>
+    hello
+</div>) ,rootEl);*/
---
+++ webpack.config.js
@@ -0,0 +1,92 @@
+'use strict';
+
+const webpack = require('webpack');
+const path = require('path');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+// const config = require('./config.js');
+const config = null;
+
+const env = process.env.NODE_ENV;
+
+const devServer = {
+  contentBase: './public',
+  historyApiFallback: true,
+  hot: true,
+  port: 3000,
+  host: '0.0.0.0',
+  stats: {
+    colors: true,
+    chunks: false,
+    chunkModules: false,
+    children: false
+  },
+// 测试环境代理。　
+//   proxy: {
+//       '/api/**': {
+//           target: config.proxy.target,
+//           changeOrigin: true
+//       }
+//   },
+  // 检查开启gzip后的大小
+  // compress: true,
+};
+
+let plugins = [
+  new webpack.HotModuleReplacementPlugin(),
+  new webpack.ContextReplacementPlugin(/moment[\\\/]locale$/, /^\.\/(en|zh-cn)$/),
+  new ExtractTextPlugin({
+    filename: 'bundle.css',
+    allChunks: true,
+  }),
+//   new HtmlWebpackPlugin({
+//     domain: config.api.crm || '',
+//     template: './templates/index.ejs',
+//     hash: true,
+//     minify: {
+//       collapseWhitespace: true,
+//       minifyJS: true,
+//       minifyCSS: true,
+//     },
+//   }),
+];
+
+module.exports = {
+  devServer,
+  // devtool: 'cheap-module-eval-source-map',
+  devtool: 'inline-source-map',
+  entry: [
+    './src/index.jsx',
+  ],
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'public'),
+    publicPath: '/',
+  },
+  module: {
+    rules: [{
+      test: /\.(js|jsx)?$/,
+      include: [
+        path.resolve(__dirname, 'src'),
+      ],
+      loader: 'babel-loader',
+    }, {
+      test: /\.(css|less)$/,
+      use: ExtractTextPlugin.extract({
+        fallback: 'style-loader',
+        use: ['css-loader', 'less-loader'],
+      }),
+    }, {
+      test: /\.(jpg|png)$/,
+      loader: 'url-loader',
+    }],
+  },
+  resolve: {
+    extensions: ['*', '.js', '.jsx'],
+    modules: [
+      path.join(__dirname, 'src'),
+      'node_modules'
+    ],
+  },
+  plugins,
+};

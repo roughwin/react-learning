c680f1e5e51c31406433a9272b4d4cbf37b18da0
bowenqing <bowenqing@innobuddy.com>
2017-05-30 18:26:09 +0800

    add jsconfig.json to project

--- src/test/fun.test.js
+++ src/test/fun.test.js
@@ -3,3 +3,20 @@ const sum = require('./fun');
 test('adds 1 + 2', () => {
   expect(sum(1, 2)).toBe(3);
 })
+
+
+
+const myMock0 = jest.fn();
+myMock0('1');
+myMock0('a', 'b');
+console.log(myMock0.mock.calls);
+
+
+// const myMock = jest.fn();
+
+const a = new myMock0();
+const b = {};
+const bound = myMock0.bind(b);
+bound();
+
+console.log(myMock0.mock.instances);

b282791ac83bb09c3dca703fbd405dc230d4f1c5
bowenqing <bowenqing@innobuddy.com>
2017-08-11 08:55:24 +0800

    g

--- src/components/common/phone_input.jsx
+++ src/components/common/phone_input.jsx
@@ -1,13 +1,31 @@
 import React, { Component, PropTypes } from 'react';
-import { Input } from 'antd';
+import { Input, Select } from 'antd';
 import _ from 'lodash';
 
+// import './phone_input.less';
+
 const InputGroup = Input.Group;
+const Option = Select.Option;
 
 class PhoneInput extends Component {
   render() {
-    return <InputGroup>
-      <Input></Input>
+    return <InputGroup compact>
+      <Select
+        defaultValue="helo"
+        dropdownMatchSelectWidth={false}
+        dropdownClassName="select-popup"
+        dropdownStyle={{
+          width: '200px',
+        }}
+      >
+        <Option value='helo'>helo0</Option>
+        <Option value='haha' title="title">haha123</Option>
+      </Select>
+      <Input
+        style={{width: '70%'}}
+      />
     </InputGroup>;
   }
 }
+
+export default PhoneInput;

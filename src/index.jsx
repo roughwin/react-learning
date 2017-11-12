f5ed6e480e6196e3816f447903d5ef8f3c845042
roughwin <roughwin@qq.com>
2017-11-12 13:05:01 +0800

    复现rc-table bug，暂无法修复

--- src/index.jsx
+++ src/index.jsx
@@ -4,37 +4,68 @@ import { render } from 'react-dom';
 import moment from 'moment';
 import { Alert, message, Select, Table, Icon, Row, Col, Input } from 'antd';
 import 'moment/locale/zh-cn';
-import {observable, action, computed, toJS} from 'mobx';
-import {observer, inject, Provider} from 'mobx-react';
 
-// import 'test/GC_debug';
-import PhoneInput from 'components/common/phone_input';
-import MyTable from 'components/common/table';
-import High from 'components/common/high';
 
-import MyWorker from "components/workers/worker.js";
 
 const Option = Select.Option;
 const rootEl = document.getElementById('root');
 
-// const check = <span><input type="checkbox"/>{'name'}</span>
-// const count = 10
+const columns = [{
+  title: 'Name',
+  dataIndex: 'name',
+  key: 'name',
+  width: 100,
+  render: text => <a href="#">{text}</a>,
+}, {
+  title: 'Age',
+  dataIndex: 'age',
+  key: 'age',
+  width: 100,
+}, {
+  title: 'Address',
+  dataIndex: 'address',
+  key: 'address',
+  width: 100,
+}, {
+  title: 'Action',
+  key: 'action',
+  width: 100,
+  fixed: 'right',
+  render: (text, record) => (
+    <span>
+      <a href="#">Action 一 {record.name}</a>
+      <span className="ant-divider" />
+      <a href="#">Delete</a>
+      <span className="ant-divider" />
+      <a href="#" className="ant-dropdown-link">
+        More actions <Icon type="down" />
+      </a>
+    </span>
+  ),
+}];
 
-// var worker = new MyWorker();
-// const worker = new MyWorker();
-// worker.postMessage({a: 1});
-// worker.onmessage = function(event) {
-//   console.log(event)
-// };
-// worker.addEventListener("message", function(event) {
-//   console.log(event);
-// });
+const data = [{
+  key: '1',
+  name: 'John Brown',
+  age: 32,
+  address: 'New York No. 1 Lake Park',
+}, {
+  key: '2',
+  name: 'Jim Green',
+  age: 42,
+  address: 'London No. 1 Lake Park',
+}, {
+  key: '3',
+  name: 'Joe Black',
+  age: 32,
+  address: 'Sidney No. 1 Lake Park',
+}];
 
-console.log(PhoneInput)
-// const Elee = High(<Input />)(PhoneInput);
 const ele = <div>
-  hello
-  <PhoneInput></PhoneInput>
+  <Table
+    columns={columns}
+    dataSource={data}
+  />
 </div>
 
   // <Router>

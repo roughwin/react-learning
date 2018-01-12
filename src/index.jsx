12935563f8f4a8e1c462b8260acb72fc1cbead67
roughwin <roughwin@qq.com>
2018-01-12 17:32:29 +0800

    add rxjs

--- src/index.jsx
+++ src/index.jsx
@@ -2,6 +2,7 @@ import React, {Component} from 'react';
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
 import { render } from 'react-dom';
 import moment from 'moment';
+import Rx from 'rxjs/Rx';
 import { Alert, message, Select, Table, Icon, Row, Col, Input } from 'antd';
 import 'moment/locale/zh-cn';
 
@@ -66,12 +67,31 @@ const ele = <div>
     dataSource={data}
   />
 </div>
-
+const ele1 = <div>
+  <input type="text"/>
+</div>
   // <Router>
   //   <Route path="/tab" render={() => {
   //       const Ele = High(PhoneInput)
   //       return <Ele />
   //     }} />
   // </Router>
+  var observable = Rx.Observable.create(function (observer) {
+    observer.next(1);
+    observer.next(2);
+    observer.next(3);
+    setTimeout(() => {
+      observer.next(4);
+      observer.complete();
+    }, 1000);
+  });
+  
+  console.log('just before subscribe');
+  observable.subscribe({
+    next: x => console.log('got value ' + x),
+    error: err => console.error('something wrong occurred: ' + err),
+    complete: () => console.log('done'),
+  });
+  console.log('just after subscribe');
 render(ele, rootEl);
 

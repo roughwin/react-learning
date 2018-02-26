f0afec2d82a86a5dfc50399703ca0bf40beafaae
roughwin <roughwin@qq.com>
2018-02-26 17:00:33 +0800

    TransitionRoutes component

--- src/components/common/routes.jsx
+++ src/components/common/routes.jsx
@@ -4,10 +4,9 @@ import { withRouter } from 'react-router'
 import { CSSTransition, TransitionGroup } from 'react-transition-group';
 import TransitionEx, { CssTransitionHOC } from 'components/common/react_transition'
 
-const RouteWithTransition = CssTransitionHOC(Route);
 
 @withRouter
-export default class Routes extends Component {
+class TransitionRoutes extends Component {
   render() {
     return <TransitionGroup
       style={{
@@ -18,7 +17,7 @@ export default class Routes extends Component {
         timeout={{
           enter: 800,
           exit: 200,
-         }}
+        }}
         key={this.props.location.pathname}
         classNames={{
           enter: 'fadeInRight',
@@ -35,13 +34,24 @@ export default class Routes extends Component {
           <Switch
             location={this.props.location}
           >
-            <Route path="/test/route2" render={() => {
-              return <div>route2</div>
-            }}/>
-            <Route path="/test/react-transition-group" component={TransitionEx} />
+           {
+             this.props.children
+           } 
           </Switch>
         </div>
       </CSSTransition>
     </TransitionGroup>
+  }
+}
+// @withRouter
+export default class Routes extends Component {
+  render() {
+    return <TransitionRoutes>
+      <Route path="/test/route2" render={() => {
+        return <div>route2</div>
+      }} />
+      <Route path="/test/react-transition-group" component={TransitionEx} />
+    </TransitionRoutes>
+
   }
 }

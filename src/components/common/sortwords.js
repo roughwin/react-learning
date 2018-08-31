592cab251294a1e80e7c9521ec5aacf1d0d05ca3
roughwin <roughwin@qq.com>
2018-08-31 14:03:22 +0800

    refine

--- src/components/common/sortwords.js
+++ src/components/common/sortwords.js
@@ -1,29 +1,32 @@
-const word = require('./word1.json');
+const wordMap = require('./word1.json');
 
 const map = {};
-Object.keys(word).forEach(k => {
-  const s = word[k].split('');
+
+
+Object.keys(wordMap).forEach(k => {
+  const s = wordMap[k].split('');
   s.forEach(c => {
     if (map[c]) {
-      map[c].push(k)
+      map[c].push(k);
     } else {
       map[c] = [k];
     }
   });
 });
 
+
+
 function genPinyin(s) {
   const r = [];
-  const sarr = s.split('')
+  const sarr = s.split('');
   sarr.forEach(c => {
     const x = map[c];
     if (x) {
-      r.push(x)
-    } else {
-      r.push([c])
+      r.push(x);
     }
+    r.push([c]);
   });
-  return r.concat(sarr);
+  return r;
 }
 
 
@@ -41,21 +44,55 @@ function trans(arrs) {
   }));
 }
 
+function isPosValid(newPos, startPos, isFirst) {
+  const [a1, b1, c1] = newPos;
+  const [a0, b0, c0] = startPos;
+  const asame = a1 === a0; const bsame = b1 === b0; const
+    csame = c1 === c0;
+  if (asame && (!bsame) && c0 >= 0) {
+    return false;
+  }
+  if (asame && bsame && csame) {
+    return false;
+  }
+  return newPos;
+}
+
+function getPos(line, char, startPos, isFirst=false) {
+  const [a0, b0, c0] = startPos;
+  let a = a0; let b = b0; let
+    c = c0;
+  for (; a < line.length; a += 1, b = 0, c = 0) {
+    const word = line[a];
+    for (; b < word.length; b += 1, c = 0) {
+      const str = word[b];
+      for (; c < str.length; c += 1) {
+        const strarr = str.split('');
+        const t = strarr[c] || '';
+        if (t.toUpperCase() === char.toUpperCase()) {
+          const result = isPosValid([a, b, c], [a0, b0, c0]);
+          if (result) return result;
+        }
+      }
+    }
+  }
+  return false;
+}
 
 /**
- * 
+ *
  * @param {*} line 一行数据
- * @param {*} test 
+ * @param {*} test
  */
 function rank(line, test) {
   const testArr = test.split('');
   let rankNum = 0;
-  let startPos = [0, 0, 0];
+  let startPos = [0, 0, -1];
   for (const c of testArr) {
     const pos = getPos(line, c, startPos);
     if (pos) {
       startPos = pos;
-      rankNum++
+      rankNum += 1;
     } else {
       return 0;
     }
@@ -63,27 +100,19 @@ function rank(line, test) {
   return rankNum;
 }
 
-function getPos(line, char, startPos) {
-  let [a, b, c] = startPos;
-  for (; a < line.length; a++ , b = 0, c = 0) {
-    const word = line[a];
-    for (; b < word.length; b++ , c = 0) {
-      const str = word[b];
-      for (; c < str.length; c++) {
-        const strarr = str.split('');
-        if (strarr[c].toUpperCase() === char.toUpperCase()) {
-          return [a, b, c];
-        }
-      }
-    }
-  }
+function test() {
+  const line = genPinyin('合作项目')
+  console.log(line)
+  const t = 'hz'
+  const r = rank(line, t)
+  console.log(r)
 }
-
+test()
 
 // 筛选
 function filter(arr, testStr) {
   const str = testStr.trim();
-  if (!str) return arr.map(a => ([1, a.line]))
+  if (!str) return arr.map(a => ([1, a.line]));
   return arr.map(l => {
     const { pinyin, line, originIndex } = l;
     const r = rank(pinyin, str);
@@ -94,5 +123,5 @@ function filter(arr, testStr) {
 module.exports = {
   trans,
   filter,
-  rank
+  rank,
 };
